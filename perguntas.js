55a57c141c359d95a7eafc6e68313b1505fca46b
salomaslucas <brasilfacciollo@gmail.com>
2024-12-02 20:22:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Papelada mortal',
+    'O que é papel?',
+    'um bagulho extraido da arvore'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia',
+    'A capital da Russia é Moscaw'
+)
+
+criaCartao(
+    'Papelada mortal',
+    'O que é uma Papelada na cara?',
+    'Uma Papelada na cara é um monte de papel na cara'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'porque em ingles é Why (uai)'
+)
